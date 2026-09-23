d5c494148713aaeb3a4d404056f19fa6d59bc1b1
Cadir Americo <cadirvalgy@gmail.com>
2026-09-23 15:20:02 +0100

    Atualizar IP local .64 (QR + script)

--- iniciar-site.bat
+++ iniciar-site.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 echo ============================================
 echo  Prestige Eyes - http://localhost:3000
-echo  Rede local: http://prestige-eyes.192.168.1.63.nip.io:3000
+echo  Rede local: http://prestige-eyes.192.168.1.64.nip.io:3000
 echo ============================================
 call node_modules\.bin\next.cmd start -H 0.0.0.0 -p 3000
 pause
